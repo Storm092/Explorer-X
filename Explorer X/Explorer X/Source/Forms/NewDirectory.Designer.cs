d905c35aba5362e70cd631181d876935e9ecdc57
Storm <92469024+Storm092@users.noreply.github.com>
2021-10-14 10:12:05 +0200

    Add files via upload

---
+++ Explorer X/Explorer X/Source/Forms/NewDirectory.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Explorer_X.Source.Forms
+{
+    partial class NewDirectory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewDirectory));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.siticoneButton2 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.path = new Siticone.UI.WinForms.SiticoneRoundedTextBox();
+            this.siticoneElipse1 = new Siticone.UI.WinForms.SiticoneElipse(this.components);
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.siticoneElipse2 = new Siticone.UI.WinForms.SiticoneElipse(this.components);
+            this.siticoneElipse3 = new Siticone.UI.WinForms.SiticoneElipse(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.siticoneRoundedTextBox1 = new Siticone.UI.WinForms.SiticoneRoundedTextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(544, 302);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.siticoneRoundedTextBox1);
+            this.panel2.Controls.Add(this.siticoneButton2);
+            this.panel2.Controls.Add(this.siticoneButton1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.path);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(544, 302);
+            this.panel2.TabIndex = 2;
+            // 
+            // siticoneButton2
+            // 
+            this.siticoneButton2.CheckedState.Parent = this.siticoneButton2;
+            this.siticoneButton2.CustomImages.Parent = this.siticoneButton2;
+            this.siticoneButton2.FillColor = System.Drawing.Color.Red;
+            this.siticoneButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton2.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton2.HoveredState.Parent = this.siticoneButton2;
+            this.siticoneButton2.Location = new System.Drawing.Point(514, 3);
+            this.siticoneButton2.Name = "siticoneButton2";
+            this.siticoneButton2.ShadowDecoration.Parent = this.siticoneButton2;
+            this.siticoneButton2.Size = new System.Drawing.Size(27, 27);
+            this.siticoneButton2.TabIndex = 15;
+            this.siticoneButton2.Text = "X";
+            this.siticoneButton2.Click += new System.EventHandler(this.siticoneButton2_Click);
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(115, 229);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(272, 45);
+            this.siticoneButton1.TabIndex = 4;
+            this.siticoneButton1.Text = "Create";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(190, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 54);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Path";
+            // 
+            // path
+            // 
+            this.path.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.path.DefaultText = "";
+            this.path.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.path.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.path.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.path.DisabledState.Parent = this.path;
+            this.path.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.path.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.path.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.path.FocusedState.Parent = this.path;
+            this.path.ForeColor = System.Drawing.Color.White;
+            this.path.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.path.HoveredState.Parent = this.path;
+            this.path.Location = new System.Drawing.Point(8, 65);
+            this.path.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.path.Name = "path";
+            this.path.PasswordChar = '\0';
+            this.path.PlaceholderText = "";
+            this.path.SelectedText = "";
+            this.path.ShadowDecoration.Parent = this.path;
+            this.path.Size = new System.Drawing.Size(510, 44);
+            this.path.TabIndex = 0;
+            // 
+            // siticoneElipse1
+            // 
+            this.siticoneElipse1.BorderRadius = 20;
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this.panel3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(544, 302);
+            this.panel3.TabIndex = 1;
+            // 
+            // siticoneElipse2
+            // 
+            this.siticoneElipse2.TargetControl = this.siticoneButton1;
+            // 
+            // siticoneElipse3
+            // 
+            this.siticoneElipse3.TargetControl = this.siticoneButton2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(190, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 54);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Name";
+            // 
+            // siticoneRoundedTextBox1
+            // 
+            this.siticoneRoundedTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.siticoneRoundedTextBox1.DefaultText = "MyDirectory";
+            this.siticoneRoundedTextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.siticoneRoundedTextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.siticoneRoundedTextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneRoundedTextBox1.DisabledState.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.siticoneRoundedTextBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.siticoneRoundedTextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneRoundedTextBox1.FocusedState.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedTextBox1.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneRoundedTextBox1.HoveredState.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.Location = new System.Drawing.Point(8, 169);
+            this.siticoneRoundedTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.siticoneRoundedTextBox1.Name = "siticoneRoundedTextBox1";
+            this.siticoneRoundedTextBox1.PasswordChar = '\0';
+            this.siticoneRoundedTextBox1.PlaceholderText = "";
+            this.siticoneRoundedTextBox1.SelectedText = "";
+            this.siticoneRoundedTextBox1.ShadowDecoration.Parent = this.siticoneRoundedTextBox1;
+            this.siticoneRoundedTextBox1.Size = new System.Drawing.Size(510, 44);
+            this.siticoneRoundedTextBox1.TabIndex = 16;
+            // 
+            // NewDirectory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 302);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NewDirectory";
+            this.Text = "NewDirectory";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private Siticone.UI.WinForms.SiticoneRoundedTextBox siticoneRoundedTextBox1;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton2;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private System.Windows.Forms.Label label2;
+        private Siticone.UI.WinForms.SiticoneRoundedTextBox path;
+        private Siticone.UI.WinForms.SiticoneElipse siticoneElipse1;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+        private Siticone.UI.WinForms.SiticoneElipse siticoneElipse2;
+        private Siticone.UI.WinForms.SiticoneElipse siticoneElipse3;
+    }
+}
---
+++ Explorer X/Explorer X/Source/Forms/NewDirectory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explorer_X.Source.Forms
+{
+    public partial class NewDirectory : Form
+    {
+        public NewDirectory()
+        {
+            InitializeComponent();
+            path.Text = $"C:\\Users\\{Environment.UserName}\\Desktop\\";
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(path.Text+siticoneRoundedTextBox1.Text);
+            Hide();
+        }
+
+        private void siticoneButton2_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+    }
+}
